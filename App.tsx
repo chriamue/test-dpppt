2de4d6c7b4cf051fa0f0fa782bcaf20441d49ebc
Christian M <chriamue@gmail.com>
2020-04-25 14:33:30 +0200

    commit code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Dpppt from 'react-native-dpppt';
 
 export default function App() {
+  Dpppt.init('APP_ID', true);
+  Dpppt.requestIgnoreBatteryOptimizations();
+  Dpppt.requestLocationAccess();
+  Dpppt.start();
+  Dpppt.status().then((status: any) => console.log(status))
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button onPress={() => Dpppt.requestIgnoreBatteryOptimizations()} title='battery' />
+      <Button onPress={() => Dpppt.status().then((status: any) => console.log(status))} title='status' />
+
+      <Button onPress={() => Dpppt.start()} title='start' />
     </View>
   );
 }
